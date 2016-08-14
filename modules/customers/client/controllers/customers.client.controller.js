0425b3763864b8aa600535eba5cf4f2823bd1b11
Ben <benlacroix83@gmail.com>
2016-08-14 14:21:19 +0200

    I'll remove admin from customer because we don't need it

--- modules/customers/client/controllers/customers.client.controller.js
+++ modules/customers/client/controllers/customers.client.controller.js
@@ -5,13 +5,45 @@
     .module('customers')
     .controller('CustomersController', CustomersController);
 
-  CustomersController.$inject = ['$scope', 'customerResolve', 'Authentication'];
+  CustomersController.$inject = ['$scope', 'customerResolve', 'Authentication', '$uibModal', '$log'];// modal and log for modal UI
 
-  function CustomersController($scope, customer, Authentication) {
+  function CustomersController($scope, customer, Authentication, $uibModal, $log) {
     var vm = this;
 
     vm.customer = customer;
     vm.authentication = Authentication;
     vm.error = null;
+
+    // Modal UI to update customer!
+    vm.animationsEnabled = true;
+    // modalUpdate is the function called from the front
+    vm.modalUpdate = function (size, selectedCustomer) {
+      var modalInstance = $uibModal.open({
+        animation: vm.animationsEnabled,
+        templateUrl: 'modules/customers/client/views/update-customer-modal.html',
+        controller: function ($scope, $uibModalInstance, customer) {
+          $scope.customer = customer;
+          $scope.cancel = function() {
+            $uibModalInstance.dismiss('cancel');
+          };
+        },
+        size: size,
+        resolve: {
+          customer: function () {
+            return selectedCustomer;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        vm.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    vm.toggleAnimation = function () {
+      vm.animationsEnabled = !vm.animationsEnabled;
+    };
   }
 }());
--- modules/customers/client/controllers/list-customers.client.controller.js
+++ modules/customers/client/controllers/list-customers.client.controller.js
@@ -5,44 +5,12 @@
     .module('customers')
     .controller('CustomersListController', CustomersListController);
 
-  CustomersListController.$inject = ['CustomersService', '$uibModal', '$log'];// modal and log for modal UI
+  CustomersListController.$inject = ['CustomersService'];
 
-  function CustomersListController(CustomersService, $uibModal, $log) {
+  function CustomersListController(CustomersService) {
     var vm = this;
 
     vm.customers = CustomersService.query();
 
-    // Modal UI to update customer!
-    vm.animationsEnabled = true;
-    // modalUpdate is the function called from the front
-    vm.modalUpdate = function (size, selectedCustomer) {
-      var modalInstance = $uibModal.open({
-        animation: vm.animationsEnabled,
-        templateUrl: 'modules/customers/client/views/customer-detail.html',
-        controller: function ($scope, $uibModalInstance, customer) {
-          $scope.customer = customer;
-
-          $scope.cancel = function() {
-            $uibModalInstance.dismiss('cancel');
-          };
-        },
-        size: size,
-        resolve: {
-          customer: function () {
-            return selectedCustomer;
-          }
-        }
-      });
-
-      modalInstance.result.then(function (selectedItem) {
-        vm.selected = selectedItem;
-      }, function () {
-        $log.info('Modal dismissed at: ' + new Date());
-      });
-    };
-
-    vm.toggleAnimation = function () {
-      vm.animationsEnabled = !vm.animationsEnabled;
-    };
   }
 }());
